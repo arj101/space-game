0bb1d13f38618df4d51063b20bc16a451ab1ae18
arj101 <arjunanmr101@gmail.com>
2024-08-18 14:27:50 +0530

    move shaders to separate file

--- public/shaders.js
+++ public/shaders.js
@@ -1,5 +1,6 @@
-const terrainShader = {
-  vertex: `
+function getShaders(width, height) {
+  const finishPlatformShader = {
+    vertex: `
 attribute vec2 position;
 attribute vec2 uv;
 varying vec2 texcoord;
@@ -14,7 +15,57 @@ void main() {
 
 `,
 
-  fragment: `
+    fragment: `
+  precision highp float;
+varying vec2 texcoord;
+varying vec2 vpos;
+uniform sampler2D texture;
+
+float rand(float n){return fract(sin(n) * 43758.5453123);}
+float rand(vec2 n) {
+	return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+}
+float noise(float p){
+	float fl = floor(p);
+  float fc = fract(p);
+	return mix(rand(fl), rand(fl + 1.0), fc);
+}
+float noise(vec2 n) {
+	const vec2 d = vec2(0.0, 1.0);
+  vec2 b = floor(n), f = smoothstep(vec2(0.0), vec2(1.0), fract(n));
+	return mix(mix(rand(b), rand(b + d.yx), f.x), mix(rand(b + d.xy), rand(b + d.yy), f.x), f.y);
+}
+
+void main() {
+   vec4 color = texture2D(texture, texcoord);
+   color *= color;
+
+  gl_FragColor = vec4(color);
+  gl_FragColor.xyz = mix(gl_FragColor.xyz, vec3(noise(texcoord * 500.0)), color.x * 0.05);
+
+  gl_FragColor.xyz += smoothstep( 0.95, 1.0, color.x) ;
+
+  gl_FragColor.a = gl_FragColor.a;
+}
+`,
+  };
+  const terrainShader = {
+    vertex: `
+attribute vec2 position;
+attribute vec2 uv;
+varying vec2 texcoord;
+varying vec2 vpos;
+uniform vec2 center;
+
+void main() {
+  gl_Position = vec4(position - center, 0., 1.);
+  texcoord = uv.xy;
+  vpos = position.xy;
+}
+
+`,
+
+    fragment_old: `
   precision highp float;
 varying vec2 texcoord;
 varying vec2 vpos;
@@ -48,7 +99,7 @@ void main() {
 }
 `,
 
-  fragmentProc: `
+    fragment: `
   precision highp float;
 varying vec2 texcoord;
 varying vec2 vpos;
@@ -141,7 +192,7 @@ vec3 voronoi(vec2 id, vec2 f) {
 }
 
 
-//	Classic Perlin 2D Noise 
+//	Classic Perlin 2D Noise
 //	by Stefan Gustavson (https://github.com/stegu/webgl-noise)
 //
 vec2 fade(vec2 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
@@ -163,7 +214,7 @@ float cnoise(vec2 P){
   vec2 g10 = vec2(gx.y,gy.y);
   vec2 g01 = vec2(gx.z,gy.z);
   vec2 g11 = vec2(gx.w,gy.w);
-  vec4 norm = 1.79284291400159 - 0.85373472095314 * 
+  vec4 norm = 1.79284291400159 - 0.85373472095314 *
     vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
   g00 *= norm.x;
   g01 *= norm.y;
@@ -255,7 +306,7 @@ void main() {
    vec4 color = texture2D(texture, texcoord);
    color *= color;
 
- 
+
    float scale = 6.0;
 
   vec2 st = texcoord * scale;
@@ -268,10 +319,10 @@ void main() {
 
   intensity = smoothstep(0.01, 0.1, intensity) - smoothstep(0.1, 0.2, intensity);
    intensity += (cn - 0.5) ;
-    
+
     intensity +=  (cnoise(st*19.0)) ;
 
-    	
+
     // color /= 9.0;
     // color = clamp(color, 0., 1.);
     // color.x = color.y = color.z = color.y;
@@ -283,10 +334,243 @@ void main() {
 
     color *= intensity;
 
- 
+
 
   gl_FragColor = vec4(color.xyz, 1.0);
 
 }
 `,
-};
+  };
+
+  const shipShader = {
+    vertex: `
+
+    attribute vec4 v_position;
+    uniform vec2 center;
+    varying vec4 position;
+    uniform float angle;
+    uniform vec2 shipCenter;
+
+    void main() {
+       vec2 ppos = v_position.xy;
+       ppos.y *= ${height.toFixed(1)}/${width.toFixed(1)};
+      vec2 pos = mat2(cos(angle), -sin(angle), sin(angle) , cos(angle)) * ppos.xy;
+      pos.y /= ${height.toFixed(1)}/${width.toFixed(1)};
+      gl_Position = vec4(pos.xy + shipCenter - center, 0., 1.);
+
+      position = v_position;
+    }
+    `,
+    fragment: `
+
+
+    precision highp float;
+    varying vec4 position;
+    uniform vec2 shipCenter;
+    uniform vec2 center;
+    uniform vec2 shipSize;
+
+    uniform float u_time;
+    uniform sampler2D img;
+    uniform sampler2D flame;
+
+    uniform vec2 lr;
+
+
+    mat2 rot(float angle) {
+      return mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
+    }
+
+    void main() {
+
+    vec2 pos = position.xy;
+    vec2 texPos = (pos.xy/shipSize.xy/2. + 1.0) * 0.5;
+
+
+
+    vec4 color = texture2D(img, texPos);
+
+ //   vec4 color = vec4(1.0 - smoothstep( 0.08, 0.09, distance(position.xy, vec2(0.) )), 1., 1., 1.);
+    gl_FragColor = color;
+
+    // gl_FragColor.xw += step(distance(texPos, vec2(0.05, 0.1)), 0.1);
+    // gl_FragColor.xw += step(distance(texPos, vec2(1.-0.05, 0.1)), 0.1);
+
+    vec2 t1 = vec2(0.00, 0.1);
+    vec2 t2 = vec2(1.-0.09, 0.1);
+
+
+    vec2 ft1 = texPos - t1;
+    vec2 ft2 = texPos - t2;
+
+    ft1 *= rot(sin(u_time * 70.) * 0.02);
+    ft2 *= rot(sin(u_time * 70.) * 0.02);
+
+    ft1.y /= abs(sin(u_time * 70. * (20. * lr.x))* (0.03 + lr.x * 0.05) + 1.);
+    ft2.y /= abs(sin(u_time * 70. * (20. * lr.y))* (0.03 + lr.y * 0.05) + 1.);
+
+
+    ft1 /= 0.1 ;
+    ft2 /= 0.1;
+
+    ft1.y *= 0.3 / (lr.x * 0.5 + 0.5);
+    ft2.y *= 0.3 / (lr.y *0.5 + 0.5);
+
+    ft1.y = 0.8 + ft1.y;
+    ft2.y = 0.85 + ft2.y;
+
+    vec4 ft1c = texture2D(flame, ft1);
+    vec4 ft2c = texture2D(flame, ft2);
+
+    float thrustFrac = 1.0 - smoothstep(0.0, 0.1, gl_FragColor.w);
+
+
+    gl_FragColor += ft1c * pow(ft1.y , 2.) * 2. * thrustFrac;
+    gl_FragColor += ft2c * pow(ft2.y , 2.) * 2. * thrustFrac;
+
+    }
+
+
+    `,
+  };
+
+  const bgShader = {
+    vertex: `
+    attribute vec4 v_position;
+    uniform vec3 center;
+    varying vec4 position;
+    uniform float u_time;
+
+    void main() {
+      gl_Position = v_position;
+      position = v_position;
+    }
+    `,
+    fragment: `
+    precision highp float;
+    varying vec4 position;
+    uniform vec3 center;
+    uniform sampler2D img;
+
+
+
+mat2 rot(float a) {
+  return mat2(
+      cos(a), -sin(a),
+      sin(a), cos(a)
+  );
+}
+
+    float noise(vec2 p) {
+      return fract(0.35353 * abs(dot(p, vec2(235658.35, 544646.464))));
+  }
+
+  float noise2(vec2 p) {
+       return fract(abs(dot(p, vec2(4648.35, 2926.464))));
+  }
+
+  float noise3(vec2 p) {
+       return fract(0.136477 * abs(dot(p, vec2(4648.35, 2926.464))));
+  }
+
+    vec3 star(vec2 id, vec2 f) {
+      vec2 sp = vec2(0.5, 0.5) - rot(noise(id) * 3.14)*vec2(0.6, 0.);
+      vec2 c = sp - f;
+
+      float size = noise3(id);
+
+      float intensity = (0.1 * size)/distance(sp, f);
+
+
+      float invlength = 0.3/length(c);
+      intensity += min(0.8, 0.002/(abs(c.y * c.x))) *  invlength;
+
+      vec2 cr = c * rot(0.785);
+
+      intensity += min(0.4, 0.002/(abs(cr.y * cr.x))) *invlength;
+      intensity = max(0., intensity - 0.01);
+
+      float red = smoothstep(0.4, 0.9, size) * size;
+      float green = smoothstep(0.2, 0.3, size) * size;
+      float blue = smoothstep(0., 0.01, size) * size;
+
+
+      vec3 sc = vec3(red,  green, blue) * intensity;
+
+      float u_time = center.z;
+
+      float blink = fract(u_time *0.05  + 353663.0* noise3(id));
+      sc *= 1.0 - step(0.98, blink);
+
+      return sc;
+  }
+
+    void main() {
+       vec2 st = position.xy + center.xy  *0.01 ;
+      st.y *= ${height.toFixed(1)}/${width.toFixed(1)};
+
+      float u_time = center.z;
+
+      st -= u_time*0.0001;
+
+      vec3 color = vec3(0.);
+
+
+      const int cutoff = 1;
+      const float scale = 6.;
+      const float star_prob = 0.9;
+      for (int x = -cutoff; x <= cutoff; x++) {
+          for (int y = -cutoff; y <= cutoff; y++) {
+
+              vec2 offset = vec2(x, y);
+              vec2 id = floor(st*scale ) + offset;
+
+               if (noise2(id) > star_prob) continue;
+
+              vec2 f = fract(st*scale) - offset  ;
+              color += star(id , f);;
+          }
+      }
+
+      color *= 0.01;
+      color = clamp(color, 0., 1.);
+
+
+
+      gl_FragColor.xyz = color;
+      // gl_FragColor.x = sin(u_time);
+      gl_FragColor.w = 1.;
+    }
+    `,
+  };
+
+  return { terrainShader, shipShader, bgShader, finishPlatformShader };
+}
+
+function compileShaders(gl, shaders) {
+  let failcount = 0;
+
+  const programs = {};
+
+  for (const shaderName in shaders) {
+    const vert = shaders[shaderName].vertex;
+    const frag = shaders[shaderName].fragment;
+
+    console.log("Compiling program: ", shaderName);
+
+    const vertCompiled = createShader(gl, gl.VERTEX_SHADER, vert);
+    const fragCompiled = createShader(gl, gl.FRAGMENT_SHADER, frag);
+
+    if (!vertCompiled || !fragCompiled) {
+      failcount++;
+      continue;
+    }
+
+    const program = createProgram(gl, vertCompiled, fragCompiled);
+    programs[shaderName] = program;
+  }
+
+  if (failcount > 0) return undefined;
+
+  return programs;
+}

415e0358b0a6a1bf7a3b3600ca55f42b0383373c
arj101 <arjunanmr101@gmail.com>
2024-08-22 06:33:53 +0530

    setup api endpoints

--- server.js
+++ server.js
@@ -1,113 +1,129 @@
 const express = require("express");
 const fs = require("fs");
 
-const app = express();
+const database = {};
 
-const SESSION_TIMEOUT = 10000; //timeout session after 10 seconds of no pings
+//Map<userID, username>
+const users = new Map();
 
-class Session {
-  constructor(playername) {
-    this.id = Math.random();
-    this.player = playername;
-    this.lastActive = Date.now();
-    this.valid = true;
-  }
-
-  sessionId() {
-    return this.id;
-  }
-
-  validateSession() {
-    if (Date.now() - this.lastActive > SESSION_TIMEOUT) {
-      this.valid = false;
-    }
-    return this.valid;
-  }
-
-  sessionValid() {
-    return this.valid;
-  }
-
-  ping() {
-    this.lastActive = Date.now();
-  }
-}
-
-function withSession(sessionID, fn) {
-  if (!sessions[sessionID]) {
-    return false;
-  }
-  return true;
-}
-
-class Player {
-  constructor(id, password) {
-    this.id = id;
-    this.password = password;
-    //level based score, and other stats
-    this.levels = [];
-    this.currentLevel = 1;
-  }
-}
-
-//Map<SessionID, Session>
+//Map<sessionID, userID>
 const sessions = new Map();
 
-//Map<PlayerID, Player>
-const players = new Map();
+//Map<sessionID, gameID>
+const gameSessions = new Map();
 
-players["foo"] = new Player("foo", "bar");
+//Map<gameID, GameSession>
+const games = new Map();
 
-// app.get("/login/", (req, res, next) => {
-//   const id = req.query.username;
-//   const passwd = req.headers.pwd;
+const app = express();
 
-//   if (!players[id]) {
-//     res.status(401).send("Invalid user");
-//     return;
-//   }
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//security by obscurity
+app.post("/:userid/:username/login", (req, res) => {
+  const userid = req.params.userid;
+  const username = req.params.username;
 
-//   const player = players[id];
+  const psd = req.headers.psd;
 
-//   if (player.password !== passwd) {
-//     res.status(401).send("Authorization failed");
-//     return;
-//   }
+  const dbuser = database.getUser(userid);
 
-//   const session = new Session(id);
-//   sessions[session.sessionId()] = session;
+  if (!dbuser) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
 
-//   res.send({ status: "success", id: session.sessionId() });
-// });
+  if (dbuser.username != -username) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
 
-// app.get("/ping/", (req, res, next) => {
-//   const id = req.headers.id;
-//   const session = sessions[id];
-//   if (session) {
-//     session.ping(req.ip);
-//     res.send({ status: "success" });
-//   }
-// });
+  if (dbuser.password !== psd) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
 
-// app.get("/assets/:asset", (req, res, next) => {
-//   console.log(req.params["asset"]);
-//   const sid = req.headers.id;
+  const sid = genSessionID();
 
-//   const session = sessions[sid];
-//   if (!session) {
-//     res.status(401).send("Invalid session");
-//     return;
-//   }
+  sessions.set(sid, userid);
 
-//   // if (!session.validateSession()) {
-//   //   res.status(401).send("Session expired");
-//   //   return;
-//   // }
+  res.send({ status: "success", sid });
+});
 
-//   return next();
-// });
+app.post("/:userid/:sessionid/gamereq/:level", (req, res) => {
+  const userid = req.params.userid;
+  const sessionid = req.params.sessionid;
+  const level = req.params.level;
+
+  if (isNaN(parseInt(level))) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  const levelnum = parseInt(level);
+
+  if (sessions[sessionid] !== userid) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  if (!users[userid]) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  if (users[userid].currlevel < levelnum) {
+    res.status(401).send("You havent reached there yet :(");
+    return;
+  }
+
+  const gameSessionID = genGameSessionID();
+  const gameSession = newGameSession({
+    id: gameSessionID,
+    level: levelnum,
+    userid,
+    timestamp: Date.now(),
+  });
+
+  gameSessions[gameSessionID] = gameSession;
+
+  res.send({ statis: "success", id: gameSessionID });
+});
+
+app.post("/:sessionid/:gamesessionid/alive/", async (req, res) => {
+  if (!sessions[req.params.sessionid]) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  if (
+    !gameSessions[req.params.sessionid] ||
+    !gameSessions[req.params.sessionid] !== req.params.gamesessionid
+  ) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  const gameSessionID = gameSessions[req.params.sessionid];
+  const gameSession = games[gameSessionID];
+
+  if (!gameSession) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  const data = req.body;
+
+  if (!data) {
+    res.status(401).send("Invalid request");
+    return;
+  }
+
+  const result = await gameSession.update(data);
+
+  if (result) res.send({ status: "success" });
+  else res.status(401).send({ status: "failed" });
+});
 
 app.use(express.static("public"));
-const userData = {};
-
 app.listen(5173, "127.0.0.1");

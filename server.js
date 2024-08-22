a04e5f8ab2e1d7fc22929272861357ce0eaf6620
arj101 <arjunanmr101@gmail.com>
2024-08-23 00:29:07 +0530

    sessions manager wip

--- server.js
+++ server.js
@@ -15,6 +15,273 @@ database.getUser = function (userid) {
   };
 };
 
+const GAME_SESSION_TIMEOUT = 15 * 1000;
+const MAX_TIMESTAMP_ERROR = 30 * 1000;
+const MAX_START_DELAY = 15 * 1000;
+const MIN_GAME_COMPLETION_TIME = 6 * 1000;
+class GameSession {
+  constructor(userID, userSessionID, gameSessionID, levelNum) {
+    this.running = true;
+    this.userID = userID;
+    this.userSessionID = userSessionID;
+    this.gameSessionID = gameSessionID;
+    this.levelNum = levelNum;
+    this.clientStarted = false;
+    this.starttimestamp = Date.now();
+    this.clientstarttimestamp = null;
+    this.pingtimestamp = Date.now();
+
+    this.eventlog = [];
+  }
+
+  ping() {
+    this.pingtimestamp = Date.now();
+  }
+
+  isActive() {
+    return Date.now() - this.pingtimestamp < GAME_SESSION_TIMEOUT;
+  }
+
+  validateEvent(rawEvent) {
+    if (!rawEvent.type) return { validEvent: false, criticalError: false };
+
+    let validEvent = true;
+    let criticalError = false;
+
+    const timestamp = rawEvent.timestamp;
+    if (!timestamp) return { validEvent: false, criticalError: false };
+
+    if (timestamp < this.starttimestamp) {
+      console.log("Invalidated game event because of timestamp inconsistency");
+      validEvent = false;
+      criticalError = true;
+    }
+
+    if (Date.now() - this.pingtimestamp > GAME_SESSION_TIMEOUT) {
+      console.log(
+        "Invalidated game event because of session timeout (didnt ping with a valid event)",
+      );
+      validEvent = false;
+      criticalError = true;
+    }
+
+    const timestampError = Math.abs(timestamp - Date.now());
+    if (timestampError > MAX_TIMESTAMP_ERROR) {
+      console.log("Invalidated game event because of timestamp inconsistency");
+      validEvent = false;
+      criticalError = true;
+    }
+
+    let parsedEvent = { timestamp, type: rawEvent.type };
+
+    switch (rawEvent.type) {
+      case "start": {
+        if (this.eventlog.length > 0 || this.clientStarted) {
+          console.log(
+            "Invalidated game session because of starting twice (or start isnt the first event to be sent)",
+          );
+          validEvent = false;
+          criticalError = true;
+        }
+        if (Math.abs(this.starttimestamp - timestamp) > MAX_START_DELAY) {
+          console.log("Invalidated game session because of starting too late");
+          validEvent = false;
+          criticalError = true;
+        }
+
+        if (!validEvent && !criticalError) {
+          this.clientstarttimestamp = timestamp;
+          this.clientStarted = true;
+        }
+        break;
+      }
+
+      //before sending finish, send an alive event with the final state
+      case "finish": {
+        if (this.eventlog.length === 0 || !this.clientStarted) {
+          console.log(
+            "Invalidated game session because of finishing without starting",
+          );
+          validEvent = false;
+          criticalError = true;
+        } else {
+          const server_game_duration = Date.now() - this.starttimestamp;
+          const client_game_duration = Math.abs(
+            timestamp - this.clientstarttimestamp,
+          );
+          const min_duration = Math.min(
+            server_game_duration,
+            client_game_duration,
+          );
+
+          if (
+            Math.abs(server_game_duration - client_game_duration) >
+            MAX_TIMESTAMP_ERROR
+          ) {
+            //huge error in game duration
+            console.log(
+              "Invalidated game session because of mismatch in game duration",
+            );
+            validEvent = false;
+            criticalError = true;
+          }
+
+          if (Math.abs(min_duration) < MIN_GAME_COMPLETION_TIME) {
+            console.log(
+              "Invalidated game session because of finishing too early",
+            );
+            validEvent = false;
+            criticalError = true;
+          }
+
+          if (!criticalError && validEvent) {
+            this.running = false;
+          }
+        }
+        break;
+      }
+
+      case "alive": {
+        if (!this.clientStarted) {
+          console.log(
+            "Invalidated game event because of sending alive event before starting",
+          );
+          validEvent = false;
+          criticalError = true;
+        } else {
+          const xpos = rawEvent.xpos;
+          const ypos = rawEvent.ypos;
+          const angle = rawEvent.angle;
+          const health = rawEvent.health;
+
+          if (!xpos || !ypos || !angle || !health) {
+            console.log(
+              "Invalidated (just) game event because of sending invalid alive event",
+            );
+            validEvent = false;
+          } else {
+            parsedEvent.xpos = xpos;
+            parsedEvent.ypos = ypos;
+            parsedEvent.angle = angle;
+            parsedEvent.health = health;
+          }
+        }
+
+        break;
+      }
+
+      //before sending dead, send an alive event with the final state
+      case "dead": {
+        if (!this.clientStarted || !this.clientstarttimestamp) {
+          console.log(
+            "Invalidated game event because of sending dead event before starting",
+          );
+          validEvent = false;
+          criticalError = true;
+        } else {
+          this.running = false;
+          //no other checks needed, no one's gonna hack the die event lol
+        }
+        break;
+      }
+
+      default:
+        validEvent = false;
+        criticalError = true;
+        console.log(
+          "Invalidated game session (not just this particular event) because of sending invalid event type (client likely doesn't know what he's doing)",
+        );
+    }
+
+    if (!validEvent && !criticalError) {
+      this.ping();
+      this.eventlog.push(parsedEvent);
+    }
+
+    return { validEvent, criticalError };
+  }
+
+  onReceiveKeepAlive(rawEvent) {
+    if (!rawEvent) return;
+    if (!rawEvent.type) return;
+    if (!this.running) return false;
+
+    const { validEvent, criticalError } = this.validateEvent(rawEvent);
+    if (criticalError) this.running = false;
+
+    return criticalError;
+  }
+
+  isValid() {
+    if (Date.now() - this.pingtimestamp > GAME_SESSION_TIMEOUT) {
+      console.log(
+        "[isValid] Invalidated game session because of session timeout",
+      );
+      return false;
+    }
+
+    return true;
+  }
+}
+
+class GameSessionsManager {
+  constructor() {
+    this.sessionGameSessionMap = new Map();
+    this.gameSessions = new Map();
+    this.sessionPoll = setInterval(() => {
+      //for each session
+      for (const [sessionID, gsid] of this.sessionGameSessionMap) {
+        const gameSession = this.gameSessions.get(gsid);
+        if (!gameSession.isValid()) {
+          console.log(
+            `Game session ${gsid} is invalid. Deleting game session.`,
+          );
+          this.deleteGameSession(sessionID, gsid);
+        }
+
+        if (!gameSession.running) {
+          console.log(`Deleting session because it has finished running`);
+          this.deleteGameSession(sessionID, gsid);
+        }
+      }
+    }, 5000);
+  }
+
+  createGameSession(sessionID) {
+    if (this.gameSessions.has(sessionID)) {
+      console.log(
+        "User already in another session (or did not exit properly). Not allowing to create another session",
+      );
+      return false;
+    }
+
+    const gameSession = new GameSession(sessionID);
+    this.gameSessions.set(sessionID, gameSession);
+  }
+
+  getGameSessionID(sessionID) {
+    return this.sessionGameSessionMap.get(sessionID);
+  }
+
+  getGameSession(gameSessionID) {
+    return this.gameSessions.get(gameSessionID);
+  }
+
+  deleteGameSession(sessionID, gameSessionID) {
+    this.sessionGameSessionMap.delete(sessionID);
+    this.gameSessions.delete(gameSessionID);
+  }
+
+  onReceiveKeepAliveAlive(gameSessionID, rawEventJSON) {
+    const gameSession = this.getGameSession(gameSessionID);
+
+    if (!rawEvent) return false;
+    if (!rawEvent.type) return false;
+
+    return gameSession.onReceiveKeepAlive(rawEvent);
+  }
+}
+
 //Map<userID, username>
 const users = new Map();
 

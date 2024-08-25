7c7d0e2f3713ca0aa90008fca81a8024e279f9e0
arj101 <arjunanmr101@gmail.com>
2024-08-25 18:34:18 +0530

    gnggdg

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ const database = {
   //This is necessary because global leaderboard is built by summing up ranks of players in each level
   levelRanklistLengthCache: new Map(),
   //number of levels in the game, probably shouldnt hard code this but should work fine
-  levelCount: 2,
+  levelCount: 3,
 
   //locks the leaderboard when either the global or level leaderboard updates are running. avoids potential corrupt leaderboards
   //each function checks if the lock is set, if it is, waits some time to recheck the lock, and only executes the function when the lock has been released (false = released)
@@ -183,7 +183,7 @@ database.buildLevelLeaderboardView = async function (level) {
     .doc(level.toString())
     .get();
 
-  const leaderboardData = leaderboardRef.data();
+  const leaderboardData = leaderboardRef.data() || {};
 
   leaderboardData.order = leaderboardData.order || [];
   leaderboardData.users = leaderboardData.users || {};

f2ee01dd7e8dc8d28fcdfdb3397cbbb6a6d9b228
arj101 <arjunanmr101@gmail.com>
2024-08-26 22:28:01 +0530

    gdjdg

--- server.js
+++ server.js
@@ -303,10 +303,12 @@ async function updateLeaderboard() {
 
       for (const userid of newOrder) {
         const user = leaderboardUsers[userid];
-        leaderboardView.push({
-          username: await database.getUsernameFromUserID(userid),
-          score: user.score || "[no score]", //i dont want to accidentally send undefined lol
-        });
+        if (user.score) {
+          leaderboardView.push({
+            username: await database.getUsernameFromUserID(userid),
+            score: user.score || "[no score]", //i dont want to accidentally send undefined lol
+          });
+        }
       }
 
       database.leaderboardViews[levelnum.toString()] = leaderboardView;
@@ -504,7 +506,7 @@ class GameSession {
     for (const event of this.eventlog) {
       if (event.type == "alive") {
         if (event.health <= 0) {
-          console.log(`[userID: ${this.userID}, health is zero`);
+          console.log(`[userID: ${this.userID}] health is zero`);
           return false;
         }
       }
@@ -583,14 +585,15 @@ class GameSession {
           validEvent = false;
           criticalError = true;
         } else {
-          const server_game_duration = Date.now() - this.starttimestamp;
+          // const server_game_duration = Date.now() - this.starttimestamp;
           const client_game_duration = Math.abs(
             timestamp - this.clientstarttimestamp,
           );
-          const min_duration = Math.min(
-            server_game_duration,
-            client_game_duration,
-          );
+          // const min_duration = Math.min(
+          //   server_game_duration,
+          //   client_game_duration,
+          // );
+          const min_duration = client_game_duration;
 
           // if (
           //   Math.abs(server_game_duration - client_game_duration) >
@@ -604,13 +607,13 @@ class GameSession {
           //   criticalError = true;
           // }
 
-          if (Math.abs(min_duration) < MIN_GAME_COMPLETION_TIME) {
-            console.log(
-              "Invalidated game session because of finishing too early",
-            );
-            validEvent = false;
-            criticalError = true;
-          }
+          // if (Math.abs(min_duration) < MIN_GAME_COMPLETION_TIME) {
+          //   console.log(
+          //     "Invalidated game session because of finishing too early",
+          //   );
+          //   validEvent = false;
+          //   criticalError = true;
+          // }
 
           if (!criticalError && validEvent) {
             this.duration = min_duration;

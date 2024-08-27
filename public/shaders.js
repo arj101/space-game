dc7f4e7b44c854a40d39c8cc6128641794ca772a
arj101 <arjunanmr101@gmail.com>
2024-08-27 10:33:04 +0530

    dont overheat gpu

--- public/shaders.js
+++ public/shaders.js
@@ -90,132 +90,17 @@ mat2 rot(float a) {
         sin(a), cos(a)
     );
 }
-
-float noise(vec2 p) {
-  return fract(3.3365334653 * abs(dot(p, vec2(235626.35, 0.46447))));
-}
-
-
-float noise2(vec2 p) {
-     return fract(abs(dot(p, vec2(4648.35, 2926.464))));
-}
-float noise3(vec2 p) {
-     return fract(0.736477 * abs(dot(p, vec2(4648.35, 2926.464))));
-}
-
-
-vec2 noisev(vec2 p) {
-  return normalize(rot(noise3(p) * 6324636.14) * vec2(noise2(p), noise(p) + 0.05));
-}
-
-
-vec2 voronoi_center(vec2 id) {
- return vec2(0.5, 0.5) - (rot(noise(id) * 3.14)*vec2(0.3, 0.)).xy;
-}
-
-vec3 voronoi(vec2 id, vec2 f) {
-  vec2 sp = voronoi_center(id);
-  vec2 c = sp - f;
-
-  vec3 color = vec3(1.);
-
-  vec2 res = vec2(4.);
-
-  float min_dist = 6.;
-
-  float f1, f2;
-  for (int x = -1; x <= 1; x++) {
-      for (int y = -1; y <=1; y++) {
-          vec2 offset = vec2(x, y);
-          vec2 sp_other = offset  + voronoi_center(id + offset);
-
-          vec2 r = f - sp_other;
-          float d = dot(r, r);
-
-          if (length(r) < min_dist) {
-              min_dist = length(r);
-          }
-
-          if (d < res.x) {
-              res.x = d;
-          }
-
-          else if (d < res.y) {
-              res.y = d;
-          }
-      }
-  }
-
-  // res += (noisev(f) - 0.5) *0.1;
-
-  // min_dist = floor( * abs(3. * sin(49.856 * atan(res.y, res.x)))) / 4.0;
-
-
- vec2 voronoi_p = (sqrt(res));
-
-  float intensity = abs(voronoi_p.y - voronoi_p.x);
-
-  intensity = floor(intensity * 10.0);
-  // intensity += floor(length(voronoi_p) * 40000.);
-
-  intensity =  smoothstep(0., 0.5, 0.001/intensity);
-  intensity += max(0., 1.0 - 0.22/pow(min_dist, 2.));
-  // intensity = smoothstep(0.1, 0., intensity);
-  // intensity = 0.004/dot(res, res);
-
-  intensity *= 1./distance(voronoi_p, f);
-  color *= clamp(0.,1., intensity);
-
-
-  return color;
-}
-
-
-//	Classic Perlin 2D Noise
-//	by Stefan Gustavson (https://github.com/stegu/webgl-noise)
 //
-vec2 fade(vec2 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
-vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
-float cnoise(vec2 P){
-  vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
-  vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
-  Pi = mod(Pi, 289.0); // To avoid truncation effects in permutation
-  vec4 ix = Pi.xzxz;
-  vec4 iy = Pi.yyww;
-  vec4 fx = Pf.xzxz;
-  vec4 fy = Pf.yyww;
-  vec4 i = permute(permute(ix) + iy);
-  vec4 gx = 2.0 * fract(i * 0.0243902439) - 1.0; // 1/41 = 0.024...
-  vec4 gy = abs(gx) - 0.5;
-  vec4 tx = floor(gx + 0.5);
-  gx = gx - tx;
-  vec2 g00 = vec2(gx.x,gy.x);
-  vec2 g10 = vec2(gx.y,gy.y);
-  vec2 g01 = vec2(gx.z,gy.z);
-  vec2 g11 = vec2(gx.w,gy.w);
-  vec4 norm = 1.79284291400159 - 0.85373472095314 *
-    vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
-  g00 *= norm.x;
-  g01 *= norm.y;
-  g10 *= norm.z;
-  g11 *= norm.w;
-  float n00 = dot(g00, vec2(fx.x, fy.x));
-  float n10 = dot(g10, vec2(fx.y, fy.y));
-  float n01 = dot(g01, vec2(fx.z, fy.z));
-  float n11 = dot(g11, vec2(fx.w, fy.w));
-  vec2 fade_xy = fade(Pf.xy);
-  vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
-  float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
-  return 2.3 * n_xy;
-}
+// Description : Array and textureless GLSL 2D simplex noise function.
+//      Author : Ian McEwan, Ashima Arts.
+//  Maintainer : stegu
+//     Lastmod : 20110822 (ijm)
+//     License : Copyright (C) 2011 Ashima Arts. All rights reserved.
+//               Distributed under the MIT License. See LICENSE file.
+//               https://github.com/ashima/webgl-noise
+//               https://github.com/stegu/webgl-noise
+//
 
-// Cellular noise ("Worley noise") in 2D in GLSL.
-// Copyright (c) Stefan Gustavson 2011-04-19. All rights reserved.
-// This code is released under the conditions of the MIT license.
-// See LICENSE file for details.
-// https://github.com/stegu/webgl-noise
-
-// Modulo 289 without a division (only multiplications)
 vec3 mod289(vec3 x) {
   return x - floor(x * (1.0 / 289.0)) * 289.0;
 }
@@ -224,22 +109,73 @@ vec2 mod289(vec2 x) {
   return x - floor(x * (1.0 / 289.0)) * 289.0;
 }
 
-// Modulo 7 without a division
-vec3 mod7(vec3 x) {
-  return x - floor(x * (1.0 / 7.0)) * 7.0;
-}
-
-// Permutation polynomial: (34x^2 + 6x) mod 289
 vec3 permute(vec3 x) {
-  return mod289((34.0 * x + 10.0) * x);
+  return mod289(((x*34.0)+10.0)*x);
 }
 
+
+vec4 mod289(vec4 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
 
 // Modulo 7 without a division
 vec4 mod7(vec4 x) {
   return x - floor(x * (1.0 / 7.0)) * 7.0;
 }
 
+// Permutation polynomial: (34x^2 + 6x) mod 289
+vec4 permute(vec4 x) {
+  return mod289((34.0 * x + 10.0) * x);
+}
+
+float snoise(vec2 v)
+  {
+  const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
+                      0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
+                     -0.577350269189626,  // -1.0 + 2.0 * C.x
+                      0.024390243902439); // 1.0 / 41.0
+// First corner
+  vec2 i  = floor(v + dot(v, C.yy) );
+  vec2 x0 = v -   i + dot(i, C.xx);
+
+// Other corners
+  vec2 i1;
+  //i1.x = step( x0.y, x0.x ); // x0.x > x0.y ? 1.0 : 0.0
+  //i1.y = 1.0 - i1.x;
+  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+  // x0 = x0 - 0.0 + 0.0 * C.xx ;
+  // x1 = x0 - i1 + 1.0 * C.xx ;
+  // x2 = x0 - 1.0 + 2.0 * C.xx ;
+  vec4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
+
+// Permutations
+  i = mod289(i); // Avoid truncation effects in permutation
+  vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+		+ i.x + vec3(0.0, i1.x, 1.0 ));
+
+  vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+  m = m*m ;
+  m = m*m ;
+
+// Gradients: 41 points uniformly over a line, mapped onto a diamond.
+// The ring size 17*17 = 289 is close to a multiple of 41 (41*7 = 287)
+
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+
+// Normalise gradients implicitly by scaling m
+// Approximation of: m *= inversesqrt( a0*a0 + h*h );
+  m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+
+// Compute final noise value at P
+  vec3 g;
+  g.x  = a0.x  * x0.x  + h.x  * x0.y;
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130.0 * dot(m, g);
+}
 
 // Cellular noise, returning F1 and F2 in a vec2.
 // Speeded up by using 2x2 search window instead of 3x3,
@@ -290,14 +226,14 @@ void main() {
   vec2 st = vpos;
   st *= 20.0;
   st.x *= ${width.toFixed(0) / height.toFixed(0)};
-  float cn = cnoise(st);
+  float cn = snoise(st);
   vec2 offset = rot(cn) * vec2(0., 0.290);
 
   vec2 f1f2 = cellular2x2(st + offset);
   float intensity = f1f2.y - f1f2.x;
 
   intensity = smoothstep(0.01, 0.1, intensity) - smoothstep(0.1, 0.2, intensity);
-   intensity += (cn - 0.5) + cnoise(st*19.0);
+   intensity += (cn - 0.5) + snoise(st*10.0);
 
 
 
@@ -331,7 +267,7 @@ void main() {
        vec2 ppos = v_position.xy;
        ppos.y *= ${height.toFixed(1)}/${width.toFixed(1)};
       vec2 pos = mat2(cos(angle), -sin(angle), sin(angle) , cos(angle)) * ppos.xy;
-      pos.y /= ${height.toFixed(1)}/${width.toFixed(1)};
+      pos.y *= ${width.toFixed(1)}/${height.toFixed(1)};
       gl_Position = vec4(pos.xy + shipCenter - center, 0., 1.);
 
       position = v_position;

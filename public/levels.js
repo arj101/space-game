23257af46506313d9c306af13c5f34a31c1782fe
arj101 <arjunanmr101@gmail.com>
2024-08-27 15:56:42 +0530

    level4

--- public/levels.js
+++ public/levels.js
@@ -8,4 +8,31 @@ const levels = {
   3: {
     filePrefix: "/levels/3/",
   },
+  4: {
+    filePrefix: "/levels/4/",
+  },
+  5: {
+    filePrefix: "/levels/5/",
+  },
+  6: {
+    filePrefix: "/levels/6/",
+  },
+  7: {
+    filePrefix: "/levels/7/",
+  },
+  8: {
+    filePrefix: "/levels/8/",
+  },
+  9: {
+    filePrefix: "/levels/9/",
+  },
+  10: {
+    filePrefix: "/levels/10/",
+  },
+  11: {
+    filePrefix: "/levels/11/",
+  },
+  12: {
+    filePrefix: "/levels/12/",
+  },
 };
